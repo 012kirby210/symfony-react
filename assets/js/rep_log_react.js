4c1cdfbcd9c78d3624d488419e149b2aa9f45b45
012kirby210 <012kirby210@gmail.com>
2024-03-03 13:12:42 +0100

    transformation createElement en jsx syntax.

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const el = React.createElement('h2',
-    null,
-    'Lift History',
-    React.createElement('span',null,'gnagna')
-
-);
-
+const el = <h2>Lift history</h2>
 
 ReactDom.render(el,document.getElementById('lift-stuff-app'));
