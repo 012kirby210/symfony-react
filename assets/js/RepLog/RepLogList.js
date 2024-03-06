dd4c6ab8682a159a032ae05a8118f76ea1899c7b
012kirby210 <012kirby210@gmail.com>
2024-03-06 18:13:19 +0100

    Transformation composant presentation en fonction presentation.

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,34 +1,30 @@
-import React, {Component} from "react";
+import React from 'react'
 
-export default class RepLogList extends Component{
-    render(){
+export default function RepLogList({ highlightedRowId, onRowClick }) {
 
-        const { highlightedRowId, onRowClick } = this.props;
 
-        const repLogs = [
-            { "id": 7, "reps": 16,"itemLabel": "My Laptop", "totalWeightLifted": 72 },
-            { "id": 8, "reps": 6, "itemLabel": "My Laptop", "totalWeightLifted": 27 },
-            { "id": 22, "reps": 5, "itemLabel": "Cat", "totalWeightLifted": 45 }
-        ];
+    const repLogs = [
+        {"id": 7, "reps": 16, "itemLabel": "My Laptop", "totalWeightLifted": 72},
+        {"id": 8, "reps": 6, "itemLabel": "My Laptop", "totalWeightLifted": 27},
+        {"id": 22, "reps": 5, "itemLabel": "Cat", "totalWeightLifted": 45}
+    ];
 
-        return (
-            <tbody>
-            {repLogs.map((repLog, index) =>
-                (
-                    <tr key={repLog.id}
-                        className={highlightedRowId === repLog.id ? 'info' : ''}
-                        onClick={() => onRowClick(repLog.id)}
-                    >
-                        <td>{repLog.itemLabel}</td>
-                        <td>{repLog.reps}</td>
-                        <td>{repLog.totalWeightLifted}</td>
-                        <td>...</td>
-                    </tr>
+    return (
+        <tbody>
+        {repLogs.map((repLog, index) =>
+            (
+                <tr key={repLog.id}
+                    className={highlightedRowId === repLog.id ? 'info' : ''}
+                    onClick={() => onRowClick(repLog.id)}
+                >
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeightLifted}</td>
+                    <td>...</td>
+                </tr>
 
-                )
-            )}
-            </tbody>
-        );
-
-    }
+            )
+        )}
+        </tbody>
+    );
 }
