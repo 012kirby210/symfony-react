6e291e8d1f3f971f84e218a525dc2a882c67807d
012kirby210 <012kirby210@gmail.com>
2024-03-09 08:05:29 +0100

    Ajout état nombre de fusée.

--- assets/js/RepLog/Replog.js
+++ assets/js/RepLog/Replog.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import RepLogList from "./RepLogList";
-import PropTypes from "prop-types";
+import PropTypes, {number} from "prop-types";
 import RepLogCreator from "./RepLogCreator";
 
 function calculateTotalWeightLifted(repLogs){
     return repLogs.reduce( (accu,repLog) => repLog.totalWeightLifted + accu, 0);
 }
 
-export default function RepLog({highlightedRowId,handleRowClick, repLogs, onAddRepLog}) {
+export default function RepLog({
+                                   highlightedRowId,
+                                   handleRowClick,
+                                   repLogs,
+                                   onAddRepLog,
+                                   numberOfChar,
+                                   onNumberOfCharChange}) {
 
     return (
         <div className="col-md-7">
             <h2>
-                Lift History
+                Lift History {`${'🚀'.repeat(numberOfChar)}`}
             </h2>
-
+            <input type={`number`} value={numberOfChar} onChange={ (event) => onNumberOfCharChange(event.target.value)}/>
             <table className="table table-striped">
                 <thead>
                 <tr>
@@ -24,7 +30,7 @@ export default function RepLog({highlightedRowId,handleRowClick, repLogs, onAddR
                     <th>&nbsp;</th>
                 </tr>
                 </thead>
-                <RepLogList highlightedRowId={highlightedRowId} onRowClick={handleRowClick} repLogs={repLogs}/>
+                <RepLogList highlightedRowId={highlightedRowId} onRowClick={handleRowClick} repLogs={repLogs} />
                 <tfoot>
                 <tr>
                     <td>&nbsp;</td>
@@ -47,5 +53,7 @@ RepLog.propTypes = {
     highlightedRowId: PropTypes.any,
     handleRowClick: PropTypes.func.isRequired,
     onAddRepLog: PropTypes.func.isRequired,
-    repLogs: PropTypes.array.isRequired
+    repLogs: PropTypes.array.isRequired,
+    numberOfChar: PropTypes.number.isRequired,
+    onNumberOfCharChange: PropTypes.func.isRequired,
 }
