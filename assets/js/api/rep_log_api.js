c1350cea51e874f641101ce73418013f55fc1186
012kirby210 <012kirby210@gmail.com>
2024-03-16 11:21:42 +0100

    Factorisation accès réseaux.

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,6 +1,14 @@
+function fetchJson(url,options){
+    return fetch(url,
+        Object.assign({credentials: 'same-origin'}, options)
+    ).then( response => {
+        return response.json();
+    })
+}
 export function getRepLogs() {
-    return fetch('/reps', {
-        credentials: 'same-origin'
-    }).then( response => response.json().
-    then( (data) => data.items) );
+    return fetchJson('/reps').then(data =>data.items);
+}
+
+export function deleteRepLog(id){
+    return fetchJson(`reps/${id}`,{method: 'DELETE'});
 }
