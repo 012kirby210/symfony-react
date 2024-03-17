ecaf8a8efc2274aba0843be4be9433746ba2682c
012kirby210 <012kirby210@gmail.com>
2024-03-17 16:20:38 +0100

    Définition des items options au plus haut dans la hiérarchie des injections.

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -6,8 +6,19 @@ const root = createRoot(document.getElementById('lift-stuff-app'));
 
 const shouldShowParameter = true;
 
+const itemOptions = [
+    { id:"cat", value:"Cat" },
+    { id:"fat_cat", value:"Big Fat Cat" },
+    { id:"laptop", value:"My Laptop" },
+    { id:"coffee_cup", value:"Coffee Cup" },
+    {id: "invalid_item", value:"some thing"},
+];
+
 root.render(
     <>
-        <RepLogApp shouldShowParameter={true}/>
+        <RepLogApp
+            shouldShowParameter={true}
+            itemOptions={itemOptions}
+        />
     </>
 );
